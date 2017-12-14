ec8d10fa7ef338031ada59c10236e0cff20efca8
Chaboude <chaboudthomas@gmail.com>
2017-12-14 23:02:58 +0100

    [FIX] fix code structure

--- app/src/main/java/fr/groupetroj/iem/projectcoderproprement/ui/activity/ComicsDetailsActivity.java
+++ app/src/main/java/fr/groupetroj/iem/projectcoderproprement/ui/activity/ComicsDetailsActivity.java
@@ -10,26 +10,49 @@ import com.squareup.picasso.Picasso;
 import fr.groupetroj.iem.projectcoderproprement.ComicsApplication;
 import fr.groupetroj.iem.projectcoderproprement.R;
 import fr.groupetroj.iem.projectcoderproprement.data.model.Comics;
+import fr.groupetroj.iem.projectcoderproprement.ui.presenter.ComicsDetailsPresenter;
+import fr.groupetroj.iem.projectcoderproprement.ui.view.ComicsDetailsView;
 
-public class ComicsDetailsActivity extends AppCompatActivity {
+public class ComicsDetailsActivity extends AppCompatActivity implements ComicsDetailsView{
+
+    TextView textView_title;
+    TextView textView_synopsis;
+    TextView textView_credits;
+    TextView textView_information;
+    ImageView imageView;
+
+    ComicsDetailsPresenter comicsDetailsPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comics_details);
 
-        TextView textView_title = (TextView) findViewById(R.id.comicsDetails_title_textView);
-        TextView textView_synopsis = (TextView) findViewById(R.id.comicsDetails_description_textView);
-        TextView textView_credits = (TextView) findViewById(R.id.comicDetails_credits_textView);
-        TextView textView_information = (TextView) findViewById(R.id.comicsDetails_informations_textView);
-        ImageView imageView = (ImageView) findViewById(R.id.comicsDetails_image);
+        initViews();
+        initializeInjection();
+        comicsDetailsPresenter.updateView();
 
-        Comics currentComics = ComicsApplication.application().getComicsManager().getCurrentComics();
+    }
 
+    private void initViews(){
+        textView_title = (TextView) findViewById(R.id.comicsDetails_title_textView);
+        textView_synopsis = (TextView) findViewById(R.id.comicsDetails_description_textView);
+        textView_credits = (TextView) findViewById(R.id.comicDetails_credits_textView);
+        textView_information = (TextView) findViewById(R.id.comicsDetails_informations_textView);
+        imageView = (ImageView) findViewById(R.id.comicsDetails_image);
+
+    }
+
+    @Override
+    public void update(Comics currentComics) {
         textView_title.setText(getString(R.string.comics_detail_page_title) + " : " + currentComics.getTitle());
         Picasso.with(this.getApplicationContext()).load(currentComics.getImageUrl()).into(imageView);
         textView_synopsis.setText(getString(R.string.comics_detail_page_synopsis) + " " + currentComics.getDescription());
         textView_credits.setText(getString(R.string.comics_detail_page_creators) + "\n" + ComicsApplication.application().getComicsManager().getCurrentComicsCreators());
         textView_information.setText(getString(R.string.comics_detail_page_informations) + " : \n" + ComicsApplication.application().getComicsManager().getCurrentComicsInformations());
+    }
+
+    private void initializeInjection(){
+        this.comicsDetailsPresenter = new ComicsDetailsPresenter(ComicsApplication.application().getComicsManager(), this, this);
     }
 }
--- app/src/main/java/fr/groupetroj/iem/projectcoderproprement/ui/activity/ComicsListActivity.java
+++ app/src/main/java/fr/groupetroj/iem/projectcoderproprement/ui/activity/ComicsListActivity.java
@@ -1,65 +1,52 @@
 package fr.groupetroj.iem.projectcoderproprement.ui.activity;
 import fr.groupetroj.iem.projectcoderproprement.ComicsApplication;
 import fr.groupetroj.iem.projectcoderproprement.data.model.Comics;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
 import java.util.ArrayList;
-
 import fr.groupetroj.iem.projectcoderproprement.R;
-import fr.groupetroj.iem.projectcoderproprement.data.async.AsyncTaskFileParsing;
+import fr.groupetroj.iem.projectcoderproprement.ui.presenter.ComicsListPresenter;
+import fr.groupetroj.iem.projectcoderproprement.ui.view.ComicsListView;
 
-public class ComicsListActivity extends AppCompatActivity {
-    ArrayList<Comics> listComics;
-    ComicsListAdapter comicsListAdapter;
+public class ComicsListActivity extends AppCompatActivity implements ComicsListView{
+    private ArrayList<Comics> listComics;
+    private ComicsListAdapter comicsListAdapter;
+    private ListView listView_comics;
+    private ComicsListPresenter comicsListPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_comics);
-        
-        final ListView listView_comics = (ListView) findViewById(R.id.listView);
-        final Context context = this;
-        
-        //Création de la ArrayList qui nous permettra de remplir la listView
-        listComics = new ArrayList<Comics>();
+        initviews();
+        initializeInjection();
+        comicsListPresenter.updateView();
+    }
 
-        comicsListAdapter = new ComicsListAdapter(listComics, this.getBaseContext());
+    public void initviews(){
+        listView_comics = (ListView) findViewById(R.id.listView);
+    }
 
-        //On attribut à notre listView l'adapter que l'on vient de créer
+    private void initializeInjection(){
+        this.comicsListPresenter = new ComicsListPresenter(ComicsApplication.application().getComicsManager(), this, this);
+    }
+
+    @Override
+    public void update(ComicsListAdapter comicsListAdapter) {
+
         listView_comics.setAdapter(comicsListAdapter);
-
-        // CHANGER LE FICHIER JSON ICI R.raw.sample_ok : le bon JSON, R.raw.sample_ko : le mauvais JSON
-
-        new AsyncTaskFileParsing().execute(listComics, comicsListAdapter, R.raw.sample_ok, this);
-
 
         //Enfin on met un écouteur d'évènement sur notre listView
         listView_comics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             @SuppressWarnings("unchecked")
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                Comics currentComics = (Comics) listView_comics.getItemAtPosition(position);
-                Context context = getApplicationContext();
-                Intent intent = new Intent(ComicsListActivity.this, ComicsDetailsActivity.class);
-                ComicsApplication.application().getComicsManager().setCurrentComics(currentComics);
-                startActivity(intent);
-
+                comicsListPresenter.goToDetailActivity(listView_comics, position);
             }
         });
-
-        listView_comics.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-
-                return true;
-            }
-        });
-
-
     }
 }
